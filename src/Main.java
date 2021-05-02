add27ef9732fef7ba27a92250bd9c374a24e26af
Kshitij Sahu <kshitij.sahu_cs19@gla.ac.in>
2021-05-02 15:20:00 +0530

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        quickSort obj = new quickSort();
+        int[] arr = {8,7,2,1,0,9,6};
+
+        int Size = arr.length;
+        quickSort run =  new quickSort();
+        run.quickSort(arr, 0 ,Size - 1);
+        System.out.println("Sorted Array:");
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/quickSort.java
+++ src/quickSort.java
@@ -1,30 +1,36 @@
+import java.util.Arrays;
+
 public class quickSort {
-    // declaring the fields
-
-    public void quickSort(int arr[], int end, int start) {
-        if (start < end) {
-
-            int pivot = mypivot(arr, start, end);
-            quickSort(arr,start,pivot-1);
-            quickSort(arr,pivot+1,end);
-
-        }
-    }
-     // pivot methode to sort the data
-
-    public int mypivot(int[] arr, int p, int q) {
-        int pivot = q;
-        int i = p - 1;
-        for (int j = p; j <= q; j++) {
-            if (arr[j] <= arr[pivot]) {
+    int mypivot(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
 
                 i++;
                 int temp = arr[i];
-                arr[i] = arr[j] = temp;
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            return i;
+        }
+
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return (i + 1);
+    }
+
+
+   void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+
+            int pivot = mypivot(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
 
         }
-        return pivot;
     }
+
+
 }
